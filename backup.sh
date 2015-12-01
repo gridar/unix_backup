a9da7a45b79c1f771605c23e08fff1e0fc252c37
Julien Girard <julien@tymate.com>
2015-12-01 16:47:32 +0100

    go through all sub directories

--- backup.sh
+++ backup.sh
@@ -1,30 +1,55 @@
 function store() {
-    printf '%q ' $@
-    echo find "$@"
-    #find $@
+    files_list="$(find $@)"
+    echo $files_list
+    tar -czf backup.tar.gz $files_list
 }
 
 function backup() {
-  directory="$1"
-  backignore="$2"
+  local src_directory="$1"
+  local backignore="$2"
 
-  # skip comments and blank lines
-  read -ra words <<< $(sed -e 's/#.*// ; /^[[:space:]]*$/d' "$backignore")
-  for word in ${words[@]}; do
-    find_arg+=" ! -name '"$word"'"
+  local directories=$(ls -l $src_directory | grep "^d" | awk '{print $9}')
+  echo directories:
+  echo $directories
+  
+  local files=$(ls -l $src_directory | grep "^-" | awk '{print $9}')
+  echo files:
+  echo $files
+
+
+  # files=grep raw_list
+  # mkdir .backup
+
+    echo i am in $src_directory
+  for directory in $directories; do
+    echo ,    go to
+    echo $src_directory/$directory
+    backup "$src_directory/$directory" "$backignore"
   done
-  find_arg+=" -print"
-  store "$directory" $find_arg
+
+  #store files
+
+
+  # # skip comments and blank lines
+  # read -ra words <<< $(sed -e 's/#.*// ; /^[[:space:]]*$/d' "$backignore")
+  # for word in ${words[@]}; do
+  #   find_arg+=" ! -name "$word
+  # done
+  # find_arg+=" -print"
+
+  #store "$directory" $find_arg
 }
 
-usage() {
-    echo "$(basename $0) [ -d directory ] [ -i ignorefile ]"
+function usage() {
+    echo "$(basename $0) [ -d directory ] [ -i backignore ]"
     exit 1
 }
 
-directory=''
-backignore='.ignorefile'    # default
 
+######## MAIN #######
+
+directory=''
+backignore='.backignore'    # default
 while getopts "i:d:h" opt ; do
   case "$opt" in
     d)  directory="$OPTARG" ;;
