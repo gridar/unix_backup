895cb3564ac8867950fbab8ed9fd97cf7359086c
DUFOUR ALEXIS <alexis.dufour@decathlon.com>
2015-12-03 20:15:43 +0100

    improvement of code

--- backup2.sh
+++ backup2.sh
@@ -1,64 +1,93 @@
 datetime=$(date +%s)
 host=$(hostname)
-backup_name="backup_"$datetime"_"$host.tar
-backup_init="backup_init.tar"
+backup_name="backup_"$datetime"_"$host.tar.gz
+backup_init="backup_init.tar.gz"
 
 
 function store() {
     local files_list=$@
-    pushd $src_directory
+    #pushd $src_directory
+    back_dir=$src_directory"/.backup/"
 
     #creation of .backup if doesn't exist
-    if ! [ -d ".backup" ];then
-      mkdir .backup
+    if ! [ -d $back_dir ];then
+      mkdir $src_directory"/.backup/"
     fi
 
-    if ! [ -f .backup/$backup_init.gz ]; then
-
+    if ! [ -f $back_dir$backup_init ]; then
       if ! [ -z $files_list ]; then
-        tar -czf .backup/$backup_init.gz $files_list
+        tar --append -C $src_directory --file=$back_dir$backup_init $files_list
+        #tar -czf .backup/$backup_init.gz $files_list
       fi
-
     else
       local txt_files=$(file -0 $files_list | sed -n '/text/p' | awk '{print $1}')
       local bin_files=$(file -0 $files_list | sed '/text/d' | awk '{print $1}')
 
-      tar -czf backup_name.gz /dev/null
-      # Archive binary files in new backup if any
-      if ! [ -z $bin_files ]; then
-        echo Archiving bin files $bin_files
-        tar -czf .backup/$backup_name.gz $bin_files
-      fi
-
-      # Unzip tar files
-
-      gunzip .backup/$backup_init.gz
-      gunzip .backup/$backup_name.gz
-
-      # Archive text files
-      for txt in $txt_files; do
-
-        local is_file_exist=$(tar -tf .backup/$backup_init | grep $txt)
-        echo $is_file_exist
-        if [ -z $is_file_exist ]; then
-
-          # Add file in backup_init then add empty file in current backup
-          tar -rf .backup/$backup_init $txt
-          touch .backup/$txt
-          tar -rf .backup/$backup_name .backup/$txt
-          rm .backup/$txt
-
+      for file in $files_list; do
+        if [ -z $(file -0 $src_directory"/"$file | sed -n '/text/p') ]; then
+            echo tar bin
+            echo $file
+            echo -----
+           tar --append -C $src_directory --file=$back_dir$backup_name $file
         else
-          echo "check diff"
-          #check diff
+          local is_file_exist=$(tar -tf $back_dir$backup_init $file)
+          echo existing file:
+          echo $is_file_exist
+          echo -------
+          if [ -z $is_file_exist ]; then
+            echo new file:
+            echo $file
+            echo -----
+            tar --append -C $src_directory --file=$back_dir$backup_init $file
+            touch $back_dir"/"$file
+            tar --append -C $back_dir --file=$back_dir$backup_name $file
+            rm $back_dir"/"$file
+          else
+            echo "check diff"
+            #check diff
+          fi
         fi
+
       done
-      
-      gzip .backup/$backup_init
-      gzip .backup/$backup_name
+
+
+      # tar --append -C $src_directory --file=$back_dir$backup_name /dev/null
+      #
+      # # Archive binary files in new backup if any
+      # if ! [ -z $bin_files ]; then
+      #   echo Archiving bin files $bin_files
+      #   tar --append -C $src_directory --file=$back_dir$backup_name $bin_files
+      # fi
+      #
+      # # Unzip tar files
+      #
+      # gunzip .backup/$backup_init
+      # gunzip .backup/$backup_name
+      #
+      # # Archive text files
+      # for txt in $txt_files; do
+      #
+      #   local is_file_exist=$(tar -tf .backup/$backup_init | grep $txt)
+      #   echo $is_file_exist
+        # if [ -z $is_file_exist ]; then
+        #
+        #   # Add file in backup_init then add empty file in current backup
+        #   tar -rf .backup/$backup_init $txt
+        #   touch .backup/$txt
+        #   tar -rf .backup/$backup_name .backup/$txt
+        #   rm .backup/$txt
+        #
+        # else
+        #   echo "check diff"
+        #   #check diff
+        # fi
+      # done
+      #
+      # gzip .backup/$backup_init
+      # gzip .backup/$backup_name
       #check diff of text file if exist in init backup push diff in new backup if not push text in init and empty file in new backup
     fi
-    popd
+
 }
 
 
