57569b12959fd56c6493dec1d922e8a2b66c09f6
DUFOUR ALEXIS <alexis.dufour@decathlon.com>
2015-12-04 14:56:09 +0100

    some modify of restore_backup

--- removebackup.sh
+++ removebackup.sh
@@ -1,4 +1,4 @@
-removebackup=$(find example_files/ -name .backup)
+removebackup=$(find example_files/ -name .backup -name .result -name .init)
 
 for i in $removebackup; do
   rm -rf $i
--- restore_backup.sh
+++ restore_backup.sh
@@ -4,32 +4,46 @@ function restore()
 {
   local output_dir=$2
 
-  local back_dir=$(direname $1)
+  local back_dir=$(dirname $1)
 
   local path_archive_file=$1
   local path_init_file=$back_dir"/"$backup_init
 
+  echo output_dir
+  echo $output_dir
+  echo ---------
+
+  echo back_dir
+  echo $back_dir
+  echo ---------
+
+
+
   if [[ ! -d $output_dir"/.restore" ]]; then
     mkdir $output_dir"/.restore"
   fi
+  tar -C $output_dir"/.restore"  -zxvf $path_archive_file
 
-  if [[ ! -d $output_dir"/.init" ]]; then
-    mkdir $output_dir"/.init"
+  if [[ ! $path_archive_file -eq $path_init_file  ]]; then
+    if [[ ! -d $output_dir"/.init" ]]; then
+      mkdir $output_dir"/.init"
+    fi
+    tar -C $output_init"/.init" -zxvf $path_init_file
   fi
 
-  tar -C $output_dir"/.restore"  -zxvf $path_archive_file
-  tar -C $output_init"/.init" -zxvf $path_init_file
 
-  $files=$(find $output_dir"/.restore" -type f -maxdepth 1  -print | sed 's/.*\///g')
-
-  for file in $files;do
-    if [[ -z $(file -0 $output_dir"/.restore/"$file | sed -n '/text/p') ]]; then
-      #binary file
-      mv $output_dir"/.restore/"$file $output_dir"/"$file
-    else
-      #text file we have to patch
-    fi
-
-  done
+  # $files=$(find $output_dir"/.restore" -type f -maxdepth 1  -print | sed 's/.*\///g')
+  #
+  # for file in $files;do
+  #   if [[ -z $(file -0 $output_dir"/.restore/"$file | sed -n '/text/p') ]]; then
+  #     #binary file
+  #     mv $output_dir"/.restore/"$file $output_dir"/"$file
+  #   else
+  #     #text file we have to patch
+  #   fi
+  #
+  # done
 
 }
+
+restore $1 $2
