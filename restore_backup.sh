4c2be49eae4bfaa9f4798820513396e60a1f8289
Julien Girard <julien@tymate.com>
2015-12-05 20:04:17 +0100

    refacto restore for second try

--- restore_backup.sh
+++ restore_backup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-backup_init="backup_init.tar.gz"
+BACKUP_INIT_FILE_NAME="backup_init.tar.gz"
 
 function restore()
 {
   local output_dir=${2-./}
   local back_dir=$(dirname ${1-./})
   local path_archive_file=$1
-  local path_init_file=$back_dir"/"$backup_init
-
+  local path_init_file=$back_dir"/"$BACKUP_INIT_FILE_NAME
 
   if [[ ! -f $path_archive_file ]]; then
     echo "Archive file doesn't exist"
@@ -19,51 +18,38 @@ function restore()
     exit 0
   fi
 
-
   if [[ ! -d $output_dir"/restore" ]]; then
     mkdir $output_dir"/restore"
   fi
 
+  
   tar -C $output_dir"/restore"  -zxvf $path_archive_file
 
   local files=$(find $output_dir"/restore" -type f -maxdepth 1 -print | sed 's/.*\///g')
+  
+  echo files
+  echo $files
 
-  if [[  $path_archive_file != $path_init_file ]]; then #if backup != init_backup
 
-    if [[ ! -d $output_dir"/.init" ]]; then
-      mkdir $output_dir"/.init"
-    fi
-
-    tar -C $output_dir"/.init" -zxvf $path_init_file
-
-    for file in $files;do
-      if [[ -z $(file -0 $output_dir"/restore/"$file | sed -n '/text/p') ]]; then
-        #binary file
-        mv $output_dir"/restore/"$file $output_dir"/"$file
-      else
-
-        if [[ -f $output_dir"/.init/"$file ]]; then
-          local patch_file=$(echo $file | cut -d . -f1)
-          mv $output_dir"/restore/"$file $output_dir"/"$patch_file.patch
-          echo -------
-          cat $output_dir"/"$patch_file.patch
-          echo -------
-          mv $output_dir"/.init/"$file $output_dir"/"$file
-          patch < $output_dir"/"$patch_file.patch
-        fi
-
-        #text file we have to patch
-      fi
-
-    done
-  else
-    for file in $files; do
-      mv $output_dir"/restore/"$file $output_dir"/"$file
-    done
+#to do for each files 
+  local file="file3.txt"
+  if [[ -z $(file -0 $output_dir"/restore/"$file | sed -n '/text/p') ]]; then
+    #if it is not a bin file
+    echo whouhouhouh
   fi
 
+  patch_datas=$(grep '^--- ' $output_dir"/restore/"$file)
+  
+  if [[ ! -z $patch_datas ]]; then
+    echo find a patch file
+    local patch_file=$file
 
-
+    #unzip initial file in BACKUP_INIT_FILE_NAME
+    #make patch
+    #delete patch file
+    #done
+  fi
+  
 
 }
 
